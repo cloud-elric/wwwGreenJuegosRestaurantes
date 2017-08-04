812b9058aacd522978ce084a3721027cf1c42335
AlfieElizondo <elizondo.alfie@gmail.com>
2017-08-03 19:52:18 -0500

    se suben certificados y se cambia texto de premio

--- views/site/verPremio.php
+++ views/site/verPremio.php
@@ -8,9 +8,13 @@ $this->title="Gracias por participar";
 <!-- Premio contenedor  -->
 	<div class="js-premio-contenedor" style="text-align:center">
 		<div class="premio js-premio-ajax">
-			<h2><?=$premio->txt_nombre?></h2>
+
+      <h3> Felicidades </h3>
+      <p>Tu habilidad te ha recompensando</p>
+      <h2><?=$premio->txt_nombre?></h2>
+      <p>No olvides imprimir tu certificado</p>
 		</div>
-    <a class="btn btn-primary" href="<?=Url::base()?>">Imprimir</a>
+    <a class="btn btn-primary" href="<?=Url::base()?>/site/imprimir-certificado?token<?=$token?>" target="_blank">Imprimir Certificado</a>
 		<a class="btn btn-secondary" href="<?=Url::base()?>">Finalizar</a>
 	</div>
 	<!-- Fin premio contenedor -->
